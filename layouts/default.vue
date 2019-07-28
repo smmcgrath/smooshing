d953257a8baa5d9f65fee1c4d5f08f42a01dda91
Your Actual Name <Your Actual Email>
2019-07-28 22:44:18 +0100

    add: nav bar & fonts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    <nuxt />
+    <app-nav/>
+    <nuxt/>
   </div>
 </template>
 
+<script>
+import AppNav from '~/components/AppNav.vue'
+
+export default {
+  components: {
+    AppNav
+  },
+}
+</script>
+
+
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+  font-family: 'Roboto', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
@@ -15,6 +27,25 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  background: darkslategrey;
+  color: #eee;
+}
+
+a, 
+a:visited,
+a:active {
+  color: white;
+  text-decoration: none;
+}
+
+h1, h2, h3 {
+  font-family: 'Alfa Slab One', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+    font-weight: normal;
+}
+
+h1 {
+  font-weight: 45px;
 }
 
 *,
@@ -22,34 +53,5 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
