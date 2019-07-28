dd9a99fe2eeb75f340719ac5da15b610b8c24d42
Your Actual Name <Your Actual Email>
2019-07-28 20:41:28 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>
+      Hello tacoface
+      <br>I love delicious tacos
+    </h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
